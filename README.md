b36c1b8ef9fc09ac2eae88fa861de7ab6fc19b40
dhanyades <dhanya.desai@gmail.com>
2025-12-15 11:30:21 -0500

    Ready for Render

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Random Dog Generator
 ### DaaS (Dogs-as-a-Service): the internet's biggest collection of open source dog pictures
 
-Submitted by: ...Rose Smith (superterp)\
-Group Members: Isabella Steinebach (isteineb), Kim Luu (...), Dhanya Desai (...)\
+Submitted by: \
+Group Members: Isabella Steinebach (isteineb), Kim Luu (...), Dhanya Desai (dhanyades)\
 App Description: Allow users to retrieve a picture of a dog and stores/retrieves all of their past dog pictures.\
 YouTube Video Link: ...\
 APIs: Dog API (https://dog.ceo/dog-api/)\
--- app.js
+++ app.js
@@ -10,19 +10,20 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-if (process.argv.length != 3) {
-  process.stdout.write(`Usage ${process.argv[1]} PORT_NUMBER`);
+// use environment PORT for Render, or command line argument for local
+let portNumber = process.env.PORT || process.argv[2];
+
+if (!portNumber) {
+  console.log(`Usage: ${process.argv[1]} PORT_NUMBER`);
+  console.log(`Or set PORT environment variable`);
   process.exit(1);
 }
-let portNumber = process.argv[2];
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", "templates");
-// app.use(express.static("public"));
-
-// let collection;
+app.use(express.static("public"));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_CONNECTION_STRING);
@@ -37,18 +38,21 @@ app.use("/", applyRouter);
 app.listen(portNumber);
 console.log(`Web server started and running at http://localhost:${portNumber}`);
 
-const prompt = "Stop to shutdown the server: ";
-process.stdout.write(prompt); 
+//  enable stdin for local development (not on Render)
+if (!process.env.PORT) {
+  const prompt = "Stop to shutdown the server: ";
+  process.stdout.write(prompt); 
 
-process.stdin.on("readable", function () {
-  const dataInput = process.stdin.read();
+  process.stdin.on("readable", function () {
+    const dataInput = process.stdin.read();
 
-  if (dataInput !== null) {
-    if (dataInput.trim() === "stop") {
-      console.log("Shutting down the server");
-      process.exit(0);
+    if (dataInput !== null) {
+      if (dataInput.trim() === "stop") {
+        console.log("Shutting down the server");
+        process.exit(0);
+      }
+      process.stdout.write(prompt);
+      process.stdin.resume();
     }
-    process.stdout.write(prompt);
-    process.stdin.resume();
-  }
-});
+  });
+}
